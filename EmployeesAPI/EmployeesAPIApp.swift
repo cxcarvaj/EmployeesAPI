947578e13eabdde34fe23103f5c3a4ebfdb0851f
cxcarvaj <cxcarvaj@gmail.com>
2025-04-10 07:18:23 -0500

    Initial Commit

---
+++ EmployeesAPI/EmployeesAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeesAPIApp.swift
+//  EmployeesAPI
+//
+//  Created by Carlos Xavier Carvajal Villegas on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct EmployeesAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
